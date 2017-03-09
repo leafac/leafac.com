fb08de484e9bd593f339c10e126672babd9acc3f
Leandro Facchinetti <me@leafac.com>
2017-03-09 08:37:20 -0500

    Edits on “Playing the Game with PLT Redex”

--- source/prose/index.html.pm
+++ source/prose/index.html.pm
@@ -2,4 +2,4 @@
 
 ◊define-meta[title]{Prose}
 
-◊section['playing-the-game-with-plt-redex]{◊link/internal["/prose/playing-the-game-with-plt-redex"]{Playing the game with PLT Redex}}
+◊section['playing-the-game-with-plt-redex]{◊link/internal["/prose/playing-the-game-with-plt-redex"]{Playing the Game with PLT Redex}}
--- source/prose/playing-the-game-with-plt-redex/index.html.pm
+++ source/prose/playing-the-game-with-plt-redex/index.html.pm
@@ -1,7 +1,7 @@
 #lang pollen
 
-◊define-meta[title]{Playing the game with PLT Redex}
-◊define-meta[date]{2017-09-03}
+◊define-meta[title]{Playing the Game with PLT Redex}
+◊define-meta[date]{2017-03-10}
 
 ◊new-thought{◊link["https://redex.racket-lang.org/"]{◊acronym{PLT} Redex} is a ◊link["https://racket-lang.org/"]{Racket} library} for semantics engineering. For people trained in programming-language theory, it is a lightweight tool to define languages, operational semantics, type systems and so on. But that is not how we are going to use it in this article. At its core, ◊acronym{PLT} Redex is a functional programming language with sophisticated pattern matching and visualization tools. And we are going to abuse these to play a game of ◊link["https://en.wikipedia.org/wiki/Peg_solitaire"]{Peg Solitaire}.
 
@@ -11,7 +11,7 @@
 
 ◊new-thought{First, we need data structures} to represent the pegs and the board. We can use a ◊emphasis{language} for this purpose. ◊acronym{PLT} Redex lets us define a grammar for a language in ◊link["https://en.wikipedia.org/wiki/Backus%E2%80%93Naur_form"]{◊acronym{BNF}} form:
 
-◊margin-note{Note that we are using Racket’s support for Unicode identifiers.}
+◊margin-note{We are using Racket’s support for Unicode identifiers.}
 
 ◊code/block{
 (define-language peg-solitaire
@@ -19,11 +19,11 @@
   (board ::= ([position ...] ...)))
 }
 
-◊margin-note{Note that, in Racket, different kinds of brackets—◊code/inline{()}, ◊code/inline{[]} and ◊code/inline{{}}—mean the same. Which one to use is a matter of readability.}
-
 The code above defines two data structures. The first, ◊code/inline{position}, represents a position in the board. It can be in one of three states: ◊code/inline{█} means uninitialized, a position that only serves as padding and is not really part of the board; ◊code/inline{○} means the position is empty; and ◊code/inline{●} means there is a peg in that position.
 
-The second data structure is the ◊code/inline{board}, represented as a 2x2 matrix of positions, or, more specifically, a list of lists of ◊code/inline{position}s. In ◊acronym{PLT} Redex, the ellipsis (“◊code/inline{...}”) means “the previous element repeated any number of times”. So ◊code/inline{[position ...]} means a list of ◊code/inline{position}s, and ◊code/inline{([position ...] ...)} means a list of lists of ◊code/inline{position}s.
+◊margin-note{In Racket, different kinds of brackets—◊code/inline{()}, ◊code/inline{[]} and ◊code/inline{{}}—mean the same. Which one to use is a matter of readability.}
+
+The second data structure is the ◊code/inline{board}, represented as a matrix of positions, or, more specifically, a list of lists of ◊code/inline{position}s. In ◊acronym{PLT} Redex, the ellipsis (◊code/inline{...}) means “the previous element repeated any number of times”. So ◊code/inline{[position ...]} means a list of ◊code/inline{position}s, and ◊code/inline{([position ...] ...)} means a list of lists of ◊code/inline{position}s.
 
 Examples of possible boards are:
 
@@ -69,9 +69,9 @@ But we are not interested in boards of ◊emphasis{any} form. Peg Solitaire’s 
      →)
 }
 
-◊margin-note{Note how closely the code resembles the ◊link["https://en.wikipedia.org/wiki/Peg_solitaire#Play"]{game specification}.}
+◊margin-note{The code closely resembles the ◊link["https://en.wikipedia.org/wiki/Peg_solitaire#Play"]{game specification}.}
 
-This rule (called ◊code/inline{→}) is saying that, if anywhere on the board there exists the sequence ◊code/inline{● ● ○}, then one possible move is for the peg on the left to jump to the right—over the peg in the middle—transforming the sequencing into ◊code/inline{○ ○ ●}.
+This rule (called ◊code/inline{→}) is saying that, if anywhere on the board there exists the sequence ◊code/inline{● ● ○}, then one possible move is for the peg on the left to jump to the right—over the peg in the middle—transforming the sequencing into ◊code/inline{○ ○ ●}.
 
 The following is the function with all the rules in the game:
 
@@ -134,7 +134,7 @@ The following is the function with all the rules in the game:
         ↑)))
 }
 
-Note that the ◊code/inline{move} function is ◊emphasis{not} performing regular pattern matching. It is not choosing the first pattern that matches, but simultaneously following all the patterns that match in parallel. One way of thinking about this is that ◊code/inline{move} is a function that returns multiple values—or, equivalently, a list of values. Another way is to think of ◊code/inline{move} as performing a non-deterministic computation. This sort of super-powered function is what ◊acronym{PLT} Redex calls a ◊technical-term{reduction relation}.
+The function ◊code/inline{move} is ◊emphasis{not} performing regular pattern matching. It is not choosing the first pattern that matches, but simultaneously following all the patterns that match in parallel. One way of thinking about this is that ◊code/inline{move} is a function that returns multiple values—or, equivalently, a list of values. Another way is to think of ◊code/inline{move} as performing a non-deterministic computation. This sort of super-powered function is what ◊acronym{PLT} Redex calls a ◊technical-term{reduction relation}.
 
 ◊;- I don't think it would be able to easily model the Game of Life, because, in that game, steps happen in parallel. It does allow us to escape to Racket, though.
 ◊;- GOAL DIRECTED SEARCH
