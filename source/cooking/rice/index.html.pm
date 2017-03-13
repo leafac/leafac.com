c937cfcc3761d5b02a30e168b604289e6406b54b
Leandro Facchinetti <me@leafac.com>
2017-03-13 09:48:45 -0400

    Fix Rice recipe

--- source/cooking/rice/index.html.pm
+++ source/cooking/rice/index.html.pm
@@ -38,18 +38,18 @@
  ◊table/body{
   ◊table/row{
    ◊table/data{1 cup}
-   ◊table/data{2 cups}
+   ◊table/data{1 ◊fraction[1 2] cups}
    ◊table/data{7 minutes on maximum then 8 minutes on medium power}
   }
   ◊table/row{
    ◊table/data{1 ◊fraction[1 2] cups}
-   ◊table/data{3 cups}
+   ◊table/data{2 ◊fraction[1 2] cups}
    ◊table/data{9 minutes on maximum then 10 minutes on medium power}
   }
   ◊table/row{
    ◊table/data{2 cups}
    ◊table/data{3 ◊fraction[1 2] cups}
-   ◊table/data{11 minutes on maximum then 12 minutes on medium power}
+   ◊table/data{11 minutes on maximum then 15 minutes on medium power}
   }
  }
 }
