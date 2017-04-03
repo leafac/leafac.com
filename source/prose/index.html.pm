2c64b22f2bb22923e1fe582055cc5649aae541ba
Leandro Facchinetti <me@leafac.com>
2017-04-03 12:52:14 -0400

    .

--- source/prose/index.html.pm
+++ source/prose/index.html.pm
@@ -2,6 +2,6 @@
 
 ◊define-meta[title]{Prose}
 
-◊section['playing-the-game-with-plt-redex]{◊link/internal["/prose/playing-the-game-with-plt-redex"]{Playing the Game with PLT Redex}}
-
 ◊section['programming-language-theory-explained-for-the-working-programmer--principles-of-programming-languages]{◊link/internal["/prose/programming-language-theory-explained-for-the-working-programmer--principles-of-programming-languages"]{Programming-Language Theory Explained for the Working Programmer: Principles of Programming Languages}}
+
+◊section['playing-the-game-with-plt-redex]{◊link/internal["/prose/playing-the-game-with-plt-redex"]{Playing the Game with PLT Redex}}
