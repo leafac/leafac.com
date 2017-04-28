4c068f476cb1a2d6473ae857f3f36cb447c2feae
Leandro Facchinetti <me@leafac.com>
2017-04-28 07:12:44 -0400

    Update documentation

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 
-(define collection "www.leafac.com")
+(define collection "www-leafac-com")
 (define deps '("base" "rackunit-lib" "pollen" "pollen-component" "css-expr" "gregor" "libuuid"))
 (define build-deps '("scribble-lib" "racket-doc"))
 (define scribblings '(("documentation/www.leafac.com.scrbl" ())))
