08aa906aa69b48f91495234584cd0b6036fb5566
Leandro Facchinetti <me@leafac.com>
2017-05-26 15:16:52 -0400

    .

---
+++ source/music/criatividade/index.html.pm
@@ -0,0 +1,109 @@
+#lang pollen
+
+◊define-meta[title]{Criatividade}
+◊define-meta[date]{2017-05-26}
+
+
+◊link["https://files.leafac.com/music/criatividade/criatividade.mp3"]{MP3} ◊link["https://files.leafac.com/music/criatividade/criatividade.zip"]{Source}
+
+◊lyrics{
+A minha criatividade
+Cria atividade
+As minhas criações
+Criam ações
+A minha criação se reflete
+No que minhas crias são
+E com a idade
+Aumenta a criativadade
+
+A
+A minha criatividade
+Cria atividade
+As minhas criações
+Criam ações
+A minha criação se reflete
+No que minhas crias são
+E com a idade
+Aumenta a criativadade
+
+F# G A
+Sem imaginação
+Eu fico sem chão
+Sem imaginação
+Eu fico sem chão
+Sem imaginação
+Eu fico sem chão
+A criatividade é minha cria
+
+E D
+
+E
+Eu me alimento
+De conhecimento
+Transformo informação
+Em inspiração
+Via transpiração
+
+O meu argumento
+É que não há atalho
+A canção
+É resultado de trabalho
+Não de piração
+
+O meu argumento
+É que conhecimento
+Cria transformação
+E o meu trabalho
+Transforma a forma
+Reação
+
+A
+A minha criatividade
+G         D
+Cria atividade
+A
+As minhas criações
+F      D
+Criam ações
+A
+A minha criação se reflete
+G                   D
+No que minhas crias são
+A
+E com a idade
+          F         G
+Aumenta a criativadade
+
+E Eb D C# C B Bb A
+A forma, em forma, informa o conteúdo
+Em forma, se forma, de forma informal
+Formula, reforma, transforma a formula
+De forma que, então, se torna real
+
+G A
+
+E Eb D C# C B Bb A
+A forma, em forma, informa o conteúdo
+Em forma, se forma, de forma informal
+Formula, reforma, transforma a formula
+De forma que, então, se torna real
+
+G A G F# F E D
+
+A
+A minha criatividade
+G         D
+Cria atividade
+A
+As minhas criações
+F      D
+Criam ações
+A
+A minha criação se reflete
+G                   D
+No que minhas crias são
+A
+E com a idade
+          F         G
+Aumenta a criativadade
+}
--- source/music/index.html.pm
+++ source/music/index.html.pm
@@ -2,6 +2,10 @@
 
 ◊define-meta[title]{Music}
 
+◊section['criatividade]{◊link/internal["/music/criatividade"]{Criatividade}}
+
+◊link["https://files.leafac.com/music/criatividade/criatividade.mp3"]{MP3} ◊link["https://files.leafac.com/music/criatividade/criatividade.zip"]{Source}
+
 ◊section['united]{◊link/internal["/music/united"]{United}}
 
 ◊link["https://files.leafac.com/music/united/united.mp3"]{MP3} ◊link["https://files.leafac.com/music/united/united.zip"]{Source}
