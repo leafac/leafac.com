b2dcaac87b5e28f8fb342f637c8a424aeac23ad2
Leandro Facchinetti <me@leafac.com>
2017-06-04 10:58:51 -0400

    .

--- source/prose/programming-language-theory-explained-for-the-working-programmer--interpretation/programming-language-theory-explained-for-the-working-programmer--interpretation.rkt
+++ source/prose/programming-language-theory-explained-for-the-working-programmer--interpretation/programming-language-theory-explained-for-the-working-programmer--interpretation.rkt
@@ -492,6 +492,145 @@
 
 ;; ENVIRONMENT-BASED INTERPRETER
 
+(module+ environment-based-interpreter
+  (define (interpret expression)
+    (define (interpret* expression environment)
+      (match expression
+        [`(closure (λ (,argument-name) ,body) ,closure-environment)
+         (closure->function expression)]
+        [_
+         (define-values (new-expression new-environment)
+           (step expression environment))
+         (interpret* new-expression new-environment)]))
+    (interpret* expression empty))
+
+  (define (step expression environment)
+    (match expression
+      [`(closure (λ (,argument-name) ,body)
+                 ,closure-environment)
+       (values expression environment)]
+      [_
+       (define-values (reduction-expression continuation)
+         (split-expression expression))
+       (define-values (reduced-expression new-environment)
+         (match reduction-expression
+           [`(λ (,argument-name) ,body)
+            (define closure
+              `(closure (λ (,argument-name) ,body)
+                        ,environment))
+            (values closure environment)]
+           [`((closure (λ (,argument-name) ,body)
+                       ,closure-environment)
+              ,argument)
+            (define new-environment
+              (dict-set closure-environment argument-name argument))
+            (values `(restore ,body ,environment) new-environment)]
+           [`(restore ,value ,previous-environment)
+            (values value previous-environment)]
+           [variable
+            (define value
+              (dict-ref environment variable))
+            (values value environment)]))
+       (values (fill-hole reduced-expression continuation)
+               new-environment)]))
+
+  (define (split-expression expression)
+    (match expression
+      [`((closure (λ (,argument-name/function) ,body/function)
+                  ,closure-environment/function)
+         (closure (λ (,argument-name/argument) ,body/argument)
+                  ,closure-environment/argument))
+       (values expression `(hole))]
+      [`((closure (λ (,argument-name/function) ,body/function)
+                  ,closure-environment/function)
+         ,argument)
+       (define-values (reduction-expression continuation)
+         (split-expression argument))
+       (values
+        reduction-expression
+        `((closure (λ (,argument-name/function) ,body/function)
+                   ,closure-environment/function)
+          ,continuation))]
+      [`(,function ,argument)
+       (define-values (reduction-expression continuation)
+         (split-expression function))
+       (values reduction-expression
+               `(,continuation ,argument))]
+      [`(restore (closure (λ (,argument-name) ,body)
+                          ,closure-environment)
+                 ,previous-environment)
+       (values expression `(hole))]
+      [`(restore ,body ,previous-environment)
+       (define-values (reduction-expression continuation)
+         (split-expression body))
+       (values reduction-expression
+               `(restore ,continuation ,previous-environment))]
+      [variable
+       (values expression `(hole))]))
+
+  (define (fill-hole program-fragment continuation)
+    (match continuation
+      [`(hole)
+       program-fragment]
+      [`(,function ,argument)
+       `(,(fill-hole program-fragment function)
+         ,(fill-hole program-fragment argument))]
+      [`(restore ,body ,previous-environment)
+       `(restore ,(fill-hole program-fragment body)
+                 ,previous-environment)]
+      [_
+       continuation]))
+
+  (define (closure->function closure)
+    (match-define
+      `(closure ,raw-function ,closure-environment)
+      closure)
+    (for/fold ([function raw-function])
+              ([binding (in-list closure-environment)])
+      (match-define `(,variable . ,raw-value) binding)
+      (define value (closure->function raw-value))
+      (substitute function variable value)))
+
+  ;; ‘substitute’ is the same as for the big-step interpreter.
+
+  (define (substitute
+           body argument-name argument)
+    (match body
+      [`(λ (,other-argument-name) ,other-body)
+       (if (equal? argument-name other-argument-name)
+           body
+           `(λ (,other-argument-name)
+              ,(substitute
+                other-body argument-name argument)))]
+      [`(,function ,other-argument)
+       `(,(substitute
+           function argument-name argument)
+         ,(substitute
+           other-argument argument-name argument))]
+      [variable
+       (if (equal? argument-name variable)
+           argument
+           variable)]))
+
+  (module+ test
+    (require rackunit (submod ".." ".." test-cases))
+
+    (check-equal? (interpret minimal-program)
+                  minimal-program/result)
+
+    (check-equal? (interpret minimal-application)
+                  minimal-application/result)
+
+    (check-equal? (interpret non-shadowing-variable-name-reuse)
+                  non-shadowing-variable-name-reuse/result)
+
+    (check-equal? (interpret shadowing)
+                  shadowing/result)))
+
+;; ---------------------------------------------------------------------------------------------------
+
+;; FACTORED-ENVIRONMENT INTERPRETER
+
 (define (interpret expression)
   (define (interpret* expression environment)
     (match expression
@@ -611,6 +750,7 @@
          argument
          variable)]))
 
+#;
 (module+ test
   (require rackunit (submod ".." test-cases))
 
@@ -624,7 +764,12 @@
                 non-shadowing-variable-name-reuse/result)
 
   (check-equal? (interpret shadowing)
-                shadowing/result))
+                shadowing/result)
+
+  (check-exn exn:fail:user? (λ () (interpret open)))
+
+  (check-equal? (interpret sum-up-to)
+                sum-up-to/result))
 
 ;; ---------------------------------------------------------------------------------------------------
 
