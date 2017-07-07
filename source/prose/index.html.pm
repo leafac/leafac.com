53f44d0ea30e54f69b33ba9e82322a26b1332402
Leandro Facchinetti <me@leafac.com>
2017-07-07 19:55:43 -0400

    Reorder articles

--- source/prose/index.html.pm
+++ source/prose/index.html.pm
@@ -2,8 +2,8 @@
 
 ◊define-meta[title]{Prose}
 
-◊section['programming-language-theory-explained-for-the-working-programmer--simple-interpreter]{◊link/internal["/prose/programming-language-theory-explained-for-the-working-programmer--simple-interpreter"]{Programming-Language Theory Explained for the Working Programmer: Simple Interpreter}}
+◊section['playing-the-game-with-plt-redex]{◊link/internal["/prose/playing-the-game-with-plt-redex"]{Playing the Game with PLT Redex}}
 
 ◊section['programming-language-theory-explained-for-the-working-programmer--principles-of-programming-languages]{◊link/internal["/prose/programming-language-theory-explained-for-the-working-programmer--principles-of-programming-languages"]{Programming-Language Theory Explained for the Working Programmer: Principles of Programming Languages}}
 
-◊section['playing-the-game-with-plt-redex]{◊link/internal["/prose/playing-the-game-with-plt-redex"]{Playing the Game with PLT Redex}}
+◊section['programming-language-theory-explained-for-the-working-programmer--simple-interpreter]{◊link/internal["/prose/programming-language-theory-explained-for-the-working-programmer--simple-interpreter"]{Programming-Language Theory Explained for the Working Programmer: Simple Interpreter}}
