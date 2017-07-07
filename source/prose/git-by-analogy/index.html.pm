ea63dc36353926ed3af43b988ed33d94fcaa43e8
Leandro Facchinetti <me@leafac.com>
2017-07-07 10:53:58 -0400

    .

--- source/prose/git-by-analogy/index.html.pm
+++ source/prose/git-by-analogy/index.html.pm
@@ -13,10 +13,34 @@
 
 ◊new-thought{Git solves} the problem of ◊technical-term{version control}. The name might sound unfamiliar, but the problem is not: keeping track of the changes in a project as it evolves, and collaborating on it with other people. The version control problem occurs whenever someone copies a file to modify it and compare the versions, sends a project as email attachment, or loses work due to a corrupted or accidentally deleted file. Git is a ◊technical-term{version control system}, which solves the version control problem by tracking the project’s history.
 
-◊; TODO: ◊margin-note{I use Git with almost everything I do on the computer: from keeping my favorite vegan recipes to the preparation of this lecture. The person below does not. «Image of filesystem with repeated files and poop.»}
+◊margin-note{
+ The author uses Git in almost everything he does on the computer: from keeping ◊link/internal["/cooking"]{vegan recipes} to the preparation of this article. The person below does not.
+
+ ◊; TODO: «Image of filesystem with copied files and poop.»
+}
 
 Most companies use some version control system, among which Git is the most popular. Also, most free-software projects use version control systems and accept contributions through them. So knowing Git is a highly marketable skill. Moreover, version control can improve overall quality of digital life: version diary entries, recipes, lecture notes, and any other personal project. No work will be lost for technical issues, and there will be a rich history of the progress over time to compare, explore and experiment with different versions of the project.
 
-◊; TODO: ◊section['how-to-start-with-git]{How to start with Git?}
+◊section['how-to-start-with-git]{How to start with Git?}
+
+◊margin-note{This article does not cover technical issues that are system-dependent and change often, for example, how to install Git and ◊acronym{GUI} front-ends. Refer to the ◊link["https://www.git-scm.com/"]{tool’s website} for more on these topics.}
+
+◊new-thought{There are two} main ways to learn Git: through the Command-Line Interface (◊acronym{CLI}) and through the Graphical User Interface (◊acronym{GUI}). The ◊acronym{GUI} serves as a front-end for the tool, it is more friendly and, for some advanced operations, it is more convenient. In day-to-day use of Git, a ◊acronym{GUI} is usually a better choice. But the ◊acronym{GUI} may not support all operations available in Git, or may have issues with big repositories. In some cases, a graphical environment is not available at all, for example, when accessing a remote server machine. Also, the ◊acronym{GUI} makes it harder to automate some tasks and generally does not interact well with scripts. So there are reasons to learn both the ◊acronym{CLI} and the ◊acronym{GUI}, as they offer different advantages and disadvantages.
+
+◊margin-note{There are stand-alone Git ◊acronym{GUI}s and there are Git ◊acronym{GUI}s inside text editors. The latter are preferred, particularly for the most frequent tasks.}
+
+In this tutorial, we show how to perform operations both with the ◊acronym{CLI} and with the ◊acronym{GUI}. While there is only one ◊acronym{CLI}, there are many choices of ◊link["https://www.git-scm.com/downloads/guis"]{◊acronym{GUI}}. The purpose of this article is communicate basic principles, which are skills that transfer to different scenarios, so we use the ◊acronym{GUI} that comes with Git. It is not the most good-looking ◊acronym{GUI}, but it is fully functional, cross-platform and requires no extra setup.
+
+For the rest of this article, we cover the most common use cases for Git, organized by three overarching goals: (1) using Git alone on a local computer; (2) using Git with remote computers; and (3) using Git with collaborators.
+
+◊section['an-aside-about-github]{An Aside About GitHub}
+
+◊margin-note{
+ ◊; TODO: «Git ≠ GitHub ⇔ Email ≠ Gmail»
+
+ GitHub is to Git as Gmail is to email. They are popular commercial tools which make the underlying technology easier to use and provide convenient extensions, but are not essential. One can use Git without GitHub the same way one can send emails from providers other than Gmail.
+}
+
+◊new-thought{It is a common} misconception among beginners that Git and ◊link["https://github.com"]{GitHub} are synonyms, or that Git is short for GitHub. They are not the same: Git is the tool, which is free software; and GitHub is a company and a commercial product built around Git which provides hosting and extended functionality. ◊reference['TODO]{In a later section}, when we discuss using Git with remote computers, we will use GitHub for the example because it is the most popular choice, but the distinction should be clear.
 
 ◊; TODO: ◊section['where-to-learn-more-about-git]{Where to Learn More About Git?}
