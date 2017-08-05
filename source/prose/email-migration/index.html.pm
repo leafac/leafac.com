dfd8473ced20ca431ac886eb8e8832a6ed2cdd94
Leandro Facchinetti <me@leafac.com>
2017-08-05 08:39:46 -0400

    .

--- source/prose/email-migration/index.html.pm
+++ source/prose/email-migration/index.html.pm
@@ -1,27 +1,27 @@
 #lang pollen
 
 ◊define-meta[title]{Email Migration: The Ultimate Solution to a Ridiculous Problem}
-◊define-meta[date]{2017-08-04}
-
-◊margin-note{◊no-indent[] I personally experienced all the issues listed here in a recent email migration.}
+◊define-meta[date]{2017-08-05}
 
 ◊margin-note{This article assumes familiarity with the command-line.}
 
-◊new-thought{Migration tools} in email clients do not work. Most applications, for example, ◊link["https://www.mozilla.org/en-US/thunderbird/"]{Thunderbird} and ◊link["https://support.apple.com/mail"]{Apple Mail}, come with assistants to import the email history from other applications. These features do not work: they lose email, concatenate all history in a single giant email, or create a new mailbox for each individual message, which break the organization system. This was supposed to be a solved problem, there are standard email exchange formats, for example, ◊code/inline{.eml} and ◊code/inline{.mbox} files. Unfortunately, every application has a slightly different understanding of those formats, so they do not solve the problem. It is a ◊emphasis{ridiculous} problem.
+◊margin-note{I personally experienced all the issues listed here in a recent email migration.}
 
-Many tools for converting between message formats exist in the Internet, but they are generally not well maintained or they are paid products. There is an alternative, simple solution, though: while email clients might not agree on the storage formats, they must all talk the same protocols to email servers! The source client can upload messages to the server, and the target client can download them. This method might not preserve tags and other client-specific advanced features. But it preserves the ◊emphasis{read} status, the folder structure, and other fundamental attributes of email archives. Moreover, this method is guaranteed to work regardless of the email clients involved.
+◊new-thought{Migration tools} in email clients do not work. Most applications, for example, ◊link["https://www.mozilla.org/en-US/thunderbird/"]{Thunderbird} and ◊link["https://support.apple.com/mail"]{Apple Mail}, come with assistants to import the email archives from other applications. These features do not work: they lose email; concatenate all history in a single giant email; create a new mailbox for each individual message, breaking the organization system; and so forth. This is frustrating, because email migration was supposed to be a solved problem: standard email exchange formats do exist, ◊code/inline{.eml} and ◊code/inline{.mbox} files being the most common examples. Unfortunately, problems arise because every application has a slightly different understanding of those formats. This is a ◊emphasis{ridiculous} problem.
 
-In fact, people on the Internet have suggested this method before. And they recommend using the email provider the person already has, for example, ◊link["https://www.google.com/gmail/"]{Gmail} or ◊link["https://www.icloud.com"]{iCloud}. This is simple to setup, because it just reuses the configuration already in the email client. For this simplicity, it is most recommended approach to people not familiar with the command-line. But it has an important drawback: it involves uploading and re-downloading the whole message history! Depending on the size of the history, this can take days. It is a ◊emphasis{ridiculous} solution.
+Besides assistants in email clients themselves, there exist many tools for converting between message formats. Unfortunately, they generally are not well maintained or paid products. However, there is an alternative, simple solution: while email clients might not agree on the storage formats, they must all talk the same protocols to email servers! The source client can upload messages to the server, and the target client can download them. This method might not preserve tags and other client-specific advanced features. But it preserves the ◊emphasis{read} status, the folder structure, and other fundamental attributes of email archives. Moreover, this method is guaranteed to work regardless of the email clients involved.
+
+In fact, people on the Internet have suggested this method before. And they recommend using the email provider the person already has, for example, ◊link["https://www.google.com/gmail/"]{Gmail} or ◊link["https://www.icloud.com"]{iCloud}. This is simple to setup, because it just reuses the configuration already in the email client. For this simplicity, it is the most recommended approach to people not familiar with the command-line. But it has an important drawback: it involves uploading and re-downloading the whole message history! Depending on the size of the history, this can take days. It is a ◊emphasis{ridiculous} solution.
 
 ◊paragraph-separation[]
 
-◊new-thought{In this article}, we introduce an alternative that brings together the two non-solutions discussed above. ◊emphasis{The ultimate solution to the ridiculous problem of email migration}. It is a compromise between running local conversion tools and using an email server to intermediate the migration: ◊emphasis{setup a local email server}.
+◊new-thought{In this article}, we introduce an alternative that brings together the two non-solutions discussed above. ◊emphasis{It is the ultimate solution to the ridiculous problem of email migration}. It is a compromise between running conversion tools locally and using a remote email server to intermediate the migration: ◊emphasis{setup a temporary local email server}.
 
 ◊full-width{◊svg{migration.svg}}
 
 ◊section['setup]{Setup}
 
-◊new-thought{Backup the mailboxes}! A poorly setup migration might result in data loss!
+◊new-thought{Backup the mailboxes}! Problems during migration might result in data loss!
 
 Install ◊link["https://www.dovecot.org"]{Dovecot}, which is our temporary email server for the migration. On ◊link["https://www.apple.com/macos/"]{macOS}, for example, use ◊link["https://brew.sh"]{Homebrew}:
 
@@ -29,7 +29,7 @@ Install ◊link["https://www.dovecot.org"]{Dovecot}, which is our temporary emai
 $ brew install dovecot
 }
 
-Create a directory to hold the temporary Dovecot’s mailboxes:
+Create a directory to hold the temporary Dovecot mailboxes:
 
 ◊code/block{
 $ mkdir ◊placeholder{migration-directory}
@@ -37,7 +37,7 @@ $ mkdir ◊placeholder{migration-directory}
 
 ◊section['configure]{Configure}
 
-◊new-thought{Configure Dovecot} with a ◊path{dovecot.conf} file, by  replacing the ◊code/inline{◊placeholder{placeholders}} on the following template:
+◊new-thought{Configure Dovecot} with a ◊path{dovecot.conf} file, by  replacing the ◊code/inline{◊placeholder{placeholders}} in the following template:
 
 ◊margin-note{See the ◊reference['dovecot-configuration-explained]{appendix} for more details on this configuration file.}
 
@@ -98,7 +98,7 @@ Choose an arbitrary ◊code/inline{◊placeholder{password}} and use the ◊path
 
   The call to ◊code/inline{sudo} is necessary because Dovecot needs to bind to a network port below 1024—specifically, the port for ◊acronym{IMAP}, 143.
 
-  The ◊code/inline{-F} flag for Dovecot makes it run on the foreground. This makes it easy to see the logs as the server runs, and to kill it with ◊keyboard{Ctrl} + ◊keyboard{C}.
+  The ◊code/inline{-F} flag for Dovecot tells it to run on the foreground, instead of becoming a daemon. This makes it easy to see the logs as the server runs, and to kill it with ◊keyboard{Ctrl} + ◊keyboard{C}.
 }
 
 ◊code/block{
@@ -109,7 +109,7 @@ Confirm that email server is running correctly by inspecting the startup log mes
 
 Connect your email clients, for example, Thunderbird and Apple Mail to this temporary email server:
 
-◊margin-note{Some email clients insist on having a server to send emails (◊acronym{SMTP}) during account creation. Let this part of the configuration fail or reuse the outgoing email configuration from other accounts.}
+◊margin-note{Some email clients insist on having a server to send emails (◊acronym{SMTP}). Let this part of the configuration fail or reuse the outgoing email configuration from other accounts.}
 
 ◊margin-note{
   If it is necessary to change Dovecot’s configuration from the ◊reference['configure]{previous section}, restart the server or run the following command on a separate terminal:
@@ -132,32 +132,37 @@ $ sudo doveadm reload
   }
 }
 
-On the source email client, move emails from the local folders to the temporary server. Wait for completion and then, on the target email client, move emails from the temporary server to local folders. Wait for completion again. To guarantee that the process succeeded, close the email clients so that they commit pending transactions with the server and check the ◊path{◊placeholder{migration-directory}}, it should contain only empty directories and Dovecot’s administration files.
+On the source email client, move emails from the local folders to the temporary server. Wait for completion and then, on the target email client, move emails from the temporary server to local folders. Wait for completion again. To guarantee that the process succeeded, close the email clients so that they commit pending transactions to the server and check the ◊path{◊placeholder{migration-directory}}, it should contain only empty directories and Dovecot’s administration files.
 
 ◊section['teardown]{Teardown}
 
-◊new-thought{After the migration} is complete, remove the configuration connecting the email clients to the temporary email server. Then, stop the server by killing the process or running the following command on a separate terminal:
+◊new-thought{After the migration} is complete, remove the configurations connecting the email clients to the temporary email server. Then, stop the server by killing the process or running the following command on a separate terminal:
 
 ◊code/block{
 $ sudo doveadm stop
 }
 
-Finally, remove the ◊path{◊placeholder{migration-directory}} and uninstall Dovecot:
+Finally, remove Dovecot’s configuration and ◊path{◊placeholder{migration-directory}}, and uninstall it:
+
+◊margin-note{Again, the location of ◊path{dovecot.conf} varies, be consistent with the choice made at ◊reference['configuration]{configuration}.}
 
 ◊code/block{
+$ rm /usr/local/etc/dovecot/dovecot.conf
 $ rm -rf ◊placeholder{migration-directory}
 $ brew uninstall dovecot
 }
 
 ◊appendix['dovecot-configuration-explained]{Dovecot configuration explained}
 
-Dovecot supports many kinds of services, for example, the ◊acronym{POP3} email server protocol and authentication for other applications. In our temporary email server, we only want the ◊acronym{IMAP} service:
+◊margin-note{Refer to Dovecot’s manual for more details on each individual configuration option.}
+
+Dovecot supports many kinds of services, for example, the ◊acronym{IMAP} and ◊acronym{POP3} email server protocols, and authentication for other applications. In our temporary email server, we only want the ◊acronym{IMAP} service:
 
 ◊code/block{
 protocols = imap
 }
 
-Usually email servers support multiple users and have to manage a database with their credentials. This database might need to collaborate with other applications, for example, a web-client which allows users to change their passwords. We do not want this complexity, so we hard-code in the configuration file the credentials for a single user, ourselves:
+Usually email servers support multiple users and have to manage a database with their credentials. This database management might need to collaborate with other applications, for example, a web-client which allows users to change their passwords. We do not want this complexity, so we hard-code in the configuration file the credentials for a single user, ourselves:
 
 ◊margin-note{Dovecot is not a single process, but a collection of processes. They perform specific tasks and drop to only the necessary privileges, which enhances security and stability. Here we configure some of these processes to run as our user, which prevents permission problems with the files and directories Dovecot generates in the ◊path{◊placeholder{migration-directory}}.}
 
