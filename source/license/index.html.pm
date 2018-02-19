73a3bb99414076eb1f8d5b1ac91e15a5fb699c99
Leandro Facchinetti <me@leafac.com>
2018-02-19 15:47:37 -0500

    Fix license

--- source/license/index.html.pm
+++ source/license/index.html.pm
@@ -4,14 +4,16 @@
 
 ◊margin-note{See the ◊link/internal["/software/www.leafac.com/"]{source code for the website}.}
 
-◊new-thought{The license} for all written material in the website is ◊link["https://www.gnu.org/licenses/fdl.html"]{◊initialism{GNU FDL}}:
+◊new-thought{The license} for all non-code content in the website is ◊link["https://www.gnu.org/licenses/fdl.html"]{◊initialism{GNU FDL}}:
 
 ◊code/block{
 Copyright (C) ◊(->string (->year (today))) ◊(dict-ref personal-data 'author).
+
 Permission is granted to copy, distribute and/or modify this document
 under the terms of the GNU Free Documentation License, Version 1.3
 or any later version published by the Free Software Foundation;
 with no Invariant Sections, no Front-Cover Texts, and no Back-Cover Texts.
+
 A copy of the license is included in the section entitled "GNU
 Free Documentation License".
 }
