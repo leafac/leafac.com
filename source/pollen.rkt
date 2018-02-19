e1cf25d474d1f7f327697021fd0e372ad976b062
Leandro Facchinetti <me@leafac.com>
2018-02-19 12:42:42 -0500

    Reorder ‘Sizes’

--- source/pollen.rkt
+++ source/pollen.rkt
@@ -68,55 +68,6 @@
 (define (feed/date) (~t (now/moment) "yyyy-MM-dd'T'HH:mm:ssxxx"))
 
 ;; ---------------------------------------------------------------------------------------------------
-;; FONTS
-
-;; TODO: Add font imports
-;;       @font-face {font-family, font-style, font-weight, font-stretch, src: url()}
-;; TODO: Remove ‘insertion’ as a class (let it be a mixin)?
-
-; 400, 400 italic, 700
-(define font/main (css-expr #:font-family "Charter" "Iowan Old Style" "Georgia" serif))
-
-; 400, 500
-(define font/monospace (css-expr #:font-family "Fira Mono" "Menlo" "Monaco" "Courier New" monospace))
-
-;; ---------------------------------------------------------------------------------------------------
-;; COLORS
-
-(define solarized
-  '((base03  . |#002b36|)
-    (base02  . |#073642|)
-    (base01  . |#586e75|)
-    (base00  . |#657b83|)
-    (base0   . |#839496|)
-    (base1   . |#93a1a1|)
-    (base2   . |#eee8d5|)
-    (base3   . |#fdf6e3|)
-    (yellow  . |#b58900|)
-    (orange  . |#cb4b16|)
-    (red     . |#dc322f|)
-    (magenta . |#d33682|)
-    (violet  . |#6c71c4|)
-    (blue    . |#268bd2|)
-    (cyan    . |#2aa198|)
-    (green   . |#859900|)))
-
-(define colorscheme
-  `((background           . ,(dict-ref solarized 'base3))
-    (background-highlight . ,(dict-ref solarized 'base2))
-    (secondary-content    . ,(dict-ref solarized 'base1))
-    (primary-content      . ,(dict-ref solarized 'base00))
-    (emphasized-content   . ,(dict-ref solarized 'base01))
-    (yellow               . ,(dict-ref solarized 'yellow))
-    (orange               . ,(dict-ref solarized 'orange))
-    (red                  . ,(dict-ref solarized 'red))
-    (magenta              . ,(dict-ref solarized 'magenta))
-    (violet               . ,(dict-ref solarized 'violet))
-    (blue                 . ,(dict-ref solarized 'blue))
-    (cyan                 . ,(dict-ref solarized 'cyan))
-    (green                . ,(dict-ref solarized 'green))))
-
-;; ---------------------------------------------------------------------------------------------------
 ;; SIZES
 
 ;; Grid
@@ -171,6 +122,55 @@
 (define size/ruler/thick/unitless 3)
 (define size/ruler/thick (css-expr px ,size/ruler/thick/unitless))
 (define size/ruler/thick/negative (css-expr px ,(- size/ruler/thick/unitless)))
+
+;; ---------------------------------------------------------------------------------------------------
+;; FONTS
+
+;; TODO: Add font imports
+;;       @font-face {font-family, font-style, font-weight, font-stretch, src: url()}
+;; TODO: Remove ‘insertion’ as a class (let it be a mixin)?
+
+; 400, 400 italic, 700
+(define font/main (css-expr #:font-family "Charter" "Iowan Old Style" "Georgia" serif))
+
+; 400, 500
+(define font/monospace (css-expr #:font-family "Fira Mono" "Menlo" "Monaco" "Courier New" monospace))
+
+;; ---------------------------------------------------------------------------------------------------
+;; COLORS
+
+(define solarized
+  '((base03  . |#002b36|)
+    (base02  . |#073642|)
+    (base01  . |#586e75|)
+    (base00  . |#657b83|)
+    (base0   . |#839496|)
+    (base1   . |#93a1a1|)
+    (base2   . |#eee8d5|)
+    (base3   . |#fdf6e3|)
+    (yellow  . |#b58900|)
+    (orange  . |#cb4b16|)
+    (red     . |#dc322f|)
+    (magenta . |#d33682|)
+    (violet  . |#6c71c4|)
+    (blue    . |#268bd2|)
+    (cyan    . |#2aa198|)
+    (green   . |#859900|)))
+
+(define colorscheme
+  `((background           . ,(dict-ref solarized 'base3))
+    (background-highlight . ,(dict-ref solarized 'base2))
+    (secondary-content    . ,(dict-ref solarized 'base1))
+    (primary-content      . ,(dict-ref solarized 'base00))
+    (emphasized-content   . ,(dict-ref solarized 'base01))
+    (yellow               . ,(dict-ref solarized 'yellow))
+    (orange               . ,(dict-ref solarized 'orange))
+    (red                  . ,(dict-ref solarized 'red))
+    (magenta              . ,(dict-ref solarized 'magenta))
+    (violet               . ,(dict-ref solarized 'violet))
+    (blue                 . ,(dict-ref solarized 'blue))
+    (cyan                 . ,(dict-ref solarized 'cyan))
+    (green                . ,(dict-ref solarized 'green))))
 
 ;; ---------------------------------------------------------------------------------------------------
 ;; COMPONENTS
