4d5d4b7268e829fd322c93c62ee6b5f959e93419
Leandro Facchinetti <me@leafac.com>
2018-03-01 14:01:52 -0500

    Add ‘News’ section

--- source/index.html.pm
+++ source/index.html.pm
@@ -7,3 +7,26 @@ I am a writer of ◊link/internal["/prose"]{prose}, ◊link/internal["/software"
 My interests are computer programming, music, books, typography, lettering, education, minimalism and ◊link/internal["/cooking"]{veganism}. I live in Baltimore, Maryland, United States.
 
 Follow my productions on the ◊link/internal["/feed.atom"]{Atom feed}. ◊link/internal["/contact"]{Contact me} via email on ◊email[◊(dict-ref personal-data 'personal-email)].
+
+◊section['news]{News}
+
+◊(require (prefix-in feed: "feed.atom.pm"))
+
+◊(apply
+  @
+  (for/list ([entry (select-from-doc 'root feed:doc)])
+    (match entry
+      [`(entry
+         (title ,title)
+         ,_ ...
+         (link ((href ,href)))
+         ,_ ...
+         (id ,id)
+         ,_ ...
+         (updated ,updated)
+         ,_ ...
+         (summary ,summary))
+       (define key (substring id (string-length "urn:uuid:")))
+       (define entry/time (substring updated 0 (string-length "yyyy-MM-dd")))
+       ◊@{◊subsection[key]{◊link[href]{◊title · ◊time{◊entry/time}}}◊summary}]
+      [_ ◊@{}])))
