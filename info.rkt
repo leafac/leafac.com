a3827391db49e84bbc8f6cc9202a92d26b5ccbf9
Leandro Facchinetti <me@leafac.com>
2018-03-03 19:42:01 -0500

    Remove dependencies

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 
 (define collection "www-leafac-com")
-(define deps '("base" "rackunit-lib" "pollen" "pollen-component" "css-expr" "gregor" "libuuid"))
+(define deps '("base" "rackunit-lib" "pollen" "gregor" "libuuid"))
 (define build-deps '("scribble-lib" "racket-doc"))
 (define scribblings '(("documentation/www.leafac.com.scrbl" ())))
 (define pkg-desc "Source for Leandro Facchinetti’s personal website")
