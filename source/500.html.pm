e67172c286b49eab88199ae0f617b98c6d863c07
Leandro Facchinetti <me@leafac.com>
2018-03-07 07:23:10 -0500

    Fix SVGs

--- source/500.html.pm
+++ source/500.html.pm
@@ -2,6 +2,6 @@
 
 ◊define-meta[title]{500 Internal Server Error}
 
-◊margin-note{◊figure/svg{/500.svg}}
+◊margin-note{◊figure/svg[◊~a{◊|settings/project-root|/500.svg}]}
 
 A serious error happened, please report it to ◊email[◊settings/email].
