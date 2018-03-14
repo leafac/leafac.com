d473d38db026fc5e9e64ca829bc5cf658c0de9a6
Leandro Facchinetti <me@leafac.com>
2018-03-14 06:39:49 -0400

    Simplify ‘index.html.pm’

--- source/index.html.pm
+++ source/index.html.pm
@@ -10,11 +10,9 @@ Follow my productions on the ◊reference["/feed.atom"]{Atom feed}. ◊reference
 
 ◊section['news]{News}
 
-◊(require (prefix-in feed: "feed.atom.pm"))
-
 ◊(apply
   @
-  (for/list ([entry (in-list (select-from-doc 'feed feed:doc))]
+  (for/list ([entry (in-list (select-from-doc 'feed "feed.atom.pm"))]
              #:when (equal? 'entry (get-tag entry)))
     (match-define
       `(entry (id ,id) (title ,title) (link ((href ,href))) (updated ,updated) (summary ,summary ...))
