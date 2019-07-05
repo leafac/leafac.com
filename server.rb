8dcb37b4135a08e86e112b249806f75418bd80ab
Leandro Facchinetti <me@leafac.com>
2019-07-05 14:45:23 -0400

    Small improvements

--- server.rb
+++ server.rb
@@ -4,8 +4,9 @@ require "dotenv/load"
 
 configure { set :github, Octokit::Client.new(access_token: ENV.fetch("GITHUB_ACCESS_TOKEN")) }
 
+before { halt 200 unless params["fax"].nil? || params["fax"].empty? }
+
 post "/newsletter" do
-  halt 200 unless params["fax"].nil? || params["fax"].empty?
   halt 400 if params["email"].nil? || params["email"].strip.empty?
   settings.github.add_comment "leafac/www.leafac.com--data", 1, "#{params["name"]&.strip} <#{params["email"].strip}>\n#{params["description"]}"
   erb "<p>Thank you for subscribing to my newsletter.</p>"
