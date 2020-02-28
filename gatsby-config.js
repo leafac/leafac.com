d9faeea3364f8c32a02c1feccc4296d7c97c76cf
Leandro Facchinetti <git@leafac.com>
2020-02-28 15:36:32 -0500

    .

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,6 +25,7 @@ module.exports = {
         ]
       }
     },
-    `gatsby-plugin-react-helmet`
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-favicon`
   ]
 };
--- src/components/layout.js
+++ src/components/layout.js
@@ -2,9 +2,9 @@ import React from "react";
 import { Helmet } from "react-helmet";
 import ReactMarkdown from "react-markdown";
 import removeMd from "remove-markdown";
-import "./layout.css";
 import "typeface-ibm-plex-serif";
 import "typeface-ibm-plex-mono";
+import "./layout.css";
 
 export default ({
   children,
@@ -22,19 +22,6 @@ export default ({
         name="description"
         content="I’m a PhD candidate in Computer Science. I’m interested in writing & reading, music & video production, running, mindfulness, minimalism, and veganism."
       />
-      <link
-        rel="icon"
-        type="image/png"
-        href="/favicon-32x32.png"
-        sizes="32x32"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        href="/favicon-16x16.png"
-        sizes="16x16"
-      />
-      <link rel="icon" type="image/x-icon" href="/favicon.ico" />
     </Helmet>
     <header>
       <h1>
