eb4b553b2fd7ec7529a165ef0a69084a2383fca5
Leandro Facchinetti <git@leafac.com>
2020-03-17 23:24:17 -0400

    .

--- src/components/layout.js
+++ src/components/layout.js
@@ -14,11 +14,6 @@ export default ({
 }) => (
   <>
     <Helmet>
-      <meta name="author" content="Leandro Facchinetti" />
-      <meta
-        name="description"
-        content="I’m a PhD candidate in Computer Science. I’m interested in writing & reading, music & video production, running, mindfulness, minimalism, and veganism."
-      />
       <title>
         {title !== undefined
           ? `${remark()
@@ -27,6 +22,11 @@ export default ({
           : ""}
         Leandro Facchinetti
       </title>
+      <meta name="author" content="Leandro Facchinetti" />
+      <meta
+        name="description"
+        content="I’m a PhD candidate in Computer Science. I’m interested in writing & reading, music & video production, running, mindfulness, minimalism, and veganism."
+      />
     </Helmet>
     <header>
       <h1>
