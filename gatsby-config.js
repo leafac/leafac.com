a10185cb6139a34fb39f456d9d4dc198beec9306
Leandro Facchinetti <git@leafac.com>
2020-04-16 00:09:00 -0400

    Prettier 2.0

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,33 +3,33 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/src/pages/`
-      }
+        path: `${__dirname}/src/pages/`,
+      },
     },
     {
       resolve: `gatsby-plugin-mdx`,
       options: {
         extensions: [`.md`],
         defaultLayouts: {
-          default: require.resolve(`./src/components/layout.js`)
+          default: require.resolve(`./src/components/layout.js`),
         },
         gatsbyRemarkPlugins: [
           {
             resolve: `gatsby-remark-vscode`,
             options: {
-              theme: `Light+ (default light)`
-            }
-          }
+              theme: `Light+ (default light)`,
+            },
+          },
         ],
         remarkPlugins: [
           require(`remark-slug`),
           require(`remark-toc`),
-          require(`remark-math`)
+          require(`remark-math`),
         ],
-        rehypePlugins: [require(`rehype-katex`)]
-      }
+        rehypePlugins: [require(`rehype-katex`)],
+      },
     },
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-favicon`
-  ]
+    `gatsby-plugin-favicon`,
+  ],
 };
