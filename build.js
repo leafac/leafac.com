cedc2f8f97f697482a719a23143890e930d46943
Leandro Facchinetti <git@leafac.com>
2020-05-03 10:52:24 -0400

    .

--- build.js
+++ build.js
@@ -23,6 +23,10 @@ const katex = require("katex");
           <meta charset="UTF-8">
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
           <title>Leandro Facchinetti</title>
+          <link rel="stylesheet" href="/styles.css">
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+          <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">      
         </head>
         <body>
           <header>
