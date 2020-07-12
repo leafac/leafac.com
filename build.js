f2590e4e873a89e4f942d42c54bc7fecc27d0fbd
Leandro Facchinetti <git@leafac.com>
2020-07-12 02:31:41 +0100

    .

--- build.js
+++ build.js
@@ -76,70 +76,83 @@ ${markdown}
 
     // Add syntax highlighting
     const highlighter = await shiki.getHighlighter({ theme: "light_plus" });
-    for (const element of document.querySelectorAll("code")) {
-      let code;
-      let language;
+    for (const element of document.querySelectorAll(
+      `pre > code[class^="language-"]`
+    )) {
+      const { language, options } = element.className.match(
+        /^language-(?<language>[a-z]+)(?<options>.*)$/
+      ).groups;
+      const code = element.textContent;
       let shouldNumberLines = false;
       let linesToHighlight = [];
-      const isBlock = element.parentElement.tagName === "PRE";
-      if (isBlock) {
-        const match = element.className.match(
-          /^language-(?<language>.*?)(?:\{(?<options>.*)\})?$/
-        );
-        if (match === null) continue;
-        code = element.textContent;
-        language = match.groups.language;
-        for (const option of (match.groups.options ?? "").split("}{"))
-          if (option === "number") shouldNumberLines = true;
-          else if (option.match(/^[0-9,\-\.]+$/))
-            linesToHighlight = rangeParser(option);
-          else
-            console.error(
-              `${htmlPath}: Unrecognized option for code block: ${option}`
-            );
-      } else {
-        const [languageSegment, ...codeSegments] = element.textContent.split(
-          "`"
-        );
-        if (codeSegments.length === 0) continue;
-        code = codeSegments.join("`");
-        language = languageSegment;
-      }
-      let highlightedCode;
+      for (const option of options.match(/(?<=\{).*?(?=\})/g) ?? [])
+        if (option === "number") shouldNumberLines = true;
+        else if (option.match(/^[0-9,\-\.]+$/))
+          linesToHighlight = rangeParser(option);
+        else console.error(`Unrecognized option for code block: ${option}`);
+      let highlightedText;
       try {
-        highlightedCode = highlighter.codeToHtml(code, language);
+        highlightedText = highlighter.codeToHtml(code, language);
       } catch (error) {
-        console.error(`${htmlPath}: ${error}`);
+        console.error(error);
         continue;
       }
-      const highlightedLines = JSDOM.fragment(highlightedCode)
-        .querySelector("code")
-        .innerHTML.split("\n");
-      if (shouldNumberLines) {
-        const width = String(highlightedLines.length).length;
-        for (const [index, line] of Object.entries(highlightedLines)) {
-          const lineNumber = String(Number(index) + 1).padStart(width);
-          highlightedLines[
-            index
-          ] = `<span style="color: #aaa;">${lineNumber}</span>  ${line}`;
+      const highlightedCode = JSDOM.fragment(highlightedText).querySelector(
+        "code"
+      );
+      const listing = JSDOM.fragment(
+        `
+          <table class="listing">
+            ${highlightedCode.innerHTML
+              .split("\n")
+              .map(
+                (line) =>
+                  `<tr><td><pre><code>${
+                    line === "" ? " " : line
+                  }</code></pre></td></tr>`
+              )
+              .join("\n")}
+          </table>
+        `
+      ).querySelector("table");
+      const lines = listing.querySelectorAll("tr");
+      if (shouldNumberLines)
+        for (const [index, line] of Object.entries(lines)) {
+          const lineNumber = Number(index) + 1;
+          line.insertAdjacentHTML(
+            "afterbegin",
+            `<td class="line-number"><pre><code>${lineNumber}</code></pre></td>`
+          );
         }
-      }
       for (const lineToHighlight of linesToHighlight) {
-        const index = lineToHighlight - 1;
-        if (highlightedLines[index] === undefined) {
+        const line = lines[lineToHighlight - 1];
+        if (line === undefined) {
           console.error(
-            `${htmlPath}: Failed to highlight line out of range: ${lineToHighlight}`
+            `Failed to highlight line out of range: ${lineToHighlight}`
           );
           continue;
         }
-        highlightedLines[
-          index
-        ] = `<div style="background-color: #e0ffff;">${highlightedLines[index]}`;
-        highlightedLines[index + 1] = `</div>${
-          highlightedLines[index + 1] ?? ""
-        }`;
+        line.classList.add("highlighted-line");
       }
-      element.innerHTML = highlightedLines.join("\n");
+      element.parentElement.outerHTML = listing.outerHTML;
+    }
+    for (const element of [
+      ...document.querySelectorAll(":not(pre) > code"),
+    ].filter((element) => element.textContent.match(/^[a-z]+`/))) {
+      const { language, code } = element.textContent.match(
+        /^(?<language>[a-z]+)`(?<code>.*)$/
+      ).groups;
+      let highlightedText;
+      try {
+        highlightedText = highlighter.codeToHtml(code, language);
+      } catch (error) {
+        console.error(error);
+        continue;
+      }
+      const highlightedCode = JSDOM.fragment(highlightedText).querySelector(
+        "code"
+      );
+      element.innerHTML = highlightedCode.innerHTML;
     }
 
     // Inline SVGs
