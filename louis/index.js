8fa9a39d7728cf5e35bf921d4c574fd8c85e57a9
Leandro Facchinetti <git@leafac.com>
2020-08-01 22:20:09 +0100

    .

--- louis/index.js
+++ louis/index.js
@@ -11,7 +11,17 @@ You’ll be redirected to PayPal now.
 2. Please don’t tick the box that says “Paying for goods or a service?” or we’d have to pay a fee.`);
   });
 
-document.querySelector("#rsvp").addEventListener("submit", (event) => {
+document.querySelector("#rsvp").addEventListener("submit", async (event) => {
   event.preventDefault();
-  alert("hi");
+  const { target } = event;
+  for (const element of target.querySelectorAll("input, button"))
+    element.disabled = true;
+  const { status } = await fetch(target.action, {
+    method: target.method,
+    body: new FormData(target),
+  });
+  target.outerHTML =
+    status === 200
+      ? `<p><strong>Thanks for RSVPing</strong></p>`
+      : `<p><strong>Oops, something went wrong. Please contact me at <a href="mailto:linda.vale.renner@gmail.com">linda.vale.renner@gmail.com</a>.</strong></p>`;
 });
