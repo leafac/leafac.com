82cb55fdec3294c247012c4807bab029bf28a792
Leandro Facchinetti <git@leafac.com>
2020-08-04 21:36:08 +0100

    .

--- playing-the-game-with-plt-redex/visualization.rkt
+++
@@ -1,8 +0,0 @@
-#lang racket
-(require redex "terms.rkt" "reduction-relations.rkt" "judgment-forms.rkt")
-
-;; > (stepper ⇨ (term initial-board))
-;; > (stepper ⇨/judgment-form (term initial-board))
-
-;; > (traces ⇨ (term initial-board))
-;; > (traces ⇨/judgment-form (term initial-board))
