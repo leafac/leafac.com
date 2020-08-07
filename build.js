985c9782a3f4dfc157c4d9ff3ff2a3983711db92
Leandro Facchinetti <git@leafac.com>
2020-08-07 11:31:28 +0100

    .

--- build.js
+++ build.js
@@ -28,7 +28,7 @@ const GitHubSlugger = require("github-slugger");
 <link rel="stylesheet" href="/styles.css">
 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">      
+<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
 
 <header>
 
