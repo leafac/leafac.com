bcc3a0de28603838c5c9d900800e4c21e92eb0d1
Leandro Facchinetti <git@leafac.com>
2020-08-09 22:56:47 +0100

    Add back optional HTML tags

--- build.js
+++ build.js
@@ -22,31 +22,31 @@ const GitHubSlugger = require("github-slugger");
     const htmlPath = markdownPath.replace(/\.md$/, ".html");
     const markdown = fs.readFileSync(markdownPath, "utf8");
     const markdownWithTemplate = `<!DOCTYPE html>
-
 <html lang="en">
+<head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
 <title>Leandro Facchinetti</title>
 <meta name="author" content="Leandro Facchinetti">
 <meta name="description" content="I’m a PhD candidate in Computer Science. I’m interested in writing & reading, music & video production, running, mindfulness, minimalism, and veganism.">
-
 <link rel="stylesheet" href="/styles.css">
 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
 <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
-
+</head>
+<body>
 <header>
 
 # [Leandro Facchinetti](/)
 
 </header>
-
 <main>
 
 ${markdown}
 
 </main>
+</body>
+</html>
 `;
     const html = remark()
       .use({
